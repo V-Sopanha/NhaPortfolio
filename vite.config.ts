c0093e698c1fa08b3c53623d185a37f31b023444
V-Sopanha <vsopanha23@gmail.com>
2025-05-12 22:25:53 +0700

    nthhh

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My_Portfolio/",
+  base: "./",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
